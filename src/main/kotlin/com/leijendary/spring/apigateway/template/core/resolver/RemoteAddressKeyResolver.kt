e6a24e6c8086a8b9a90f3df3e26f4fed3644a2e8
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2023-02-25 18:02:55 +0100

    better remote address key resolver using XForwarded

--- src/main/kotlin/com/leijendary/spring/apigateway/template/core/resolver/RemoteAddressKeyResolver.kt
+++ src/main/kotlin/com/leijendary/spring/apigateway/template/core/resolver/RemoteAddressKeyResolver.kt
@@ -1,14 +1,20 @@
 package com.leijendary.spring.apigateway.template.core.resolver
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver
+import org.springframework.cloud.gateway.support.ipresolver.XForwardedRemoteAddressResolver
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
-import reactor.core.publisher.Mono.justOrEmpty
+import reactor.core.publisher.Mono.just
 
 @Component
 class RemoteAddressKeyResolver : KeyResolver {
+    private val resolver = XForwardedRemoteAddressResolver.maxTrustedIndex(1)
+
     override fun resolve(exchange: ServerWebExchange): Mono<String> {
-        return justOrEmpty(exchange.request.remoteAddress?.toString())
+        val socketAddress = resolver.resolve(exchange)
+        val hostAddress = socketAddress.address.hostAddress
+
+        return just(hostAddress)
     }
 }
