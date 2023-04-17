f064ed9dbf93fb8097d5718c76b2015328d8c945
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2023-04-17 21:03:12 +0200

    Removed kotlin coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,6 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation(kotlin("test"))
 
-    // Kotlinx
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-
     // Spring Boot Starter
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
