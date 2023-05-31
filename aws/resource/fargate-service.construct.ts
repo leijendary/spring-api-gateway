5871625e4fb8389ff308c95e434e40d7e88aaf58
Jonathan Leijendekker <jonathanleijendekker@gmail.com>
2023-05-31 10:09:26 +0200

    Adjusted healthCheckGracePeriod to 100 for non-prod

--- aws/resource/fargate-service.construct.ts
+++ aws/resource/fargate-service.construct.ts
@@ -37,7 +37,7 @@ export class FargateServiceConstruct extends FargateService {
       serviceName: name,
       securityGroups: [securityGroup],
       taskDefinition,
-      healthCheckGracePeriod: Duration.seconds(isProd() ? 0 : 200),
+      healthCheckGracePeriod: Duration.seconds(isProd() ? 0 : 100),
       minHealthyPercent: 100,
       maxHealthyPercent: 200,
       desiredCount: 1,
